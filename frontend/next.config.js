5d0b841b1364ecba5d3b0243fbef63b9c9b61408
riteshjha8 <riteshjha689@gmail.com>
2026-08-12 00:47:21 +0530

    server issues

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,7 +7,11 @@ const nextConfig = {
     root: path.resolve(__dirname, '..'),
   },
   async rewrites() {
-    const apiHost = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+  const apiHost =
+  process.env.NEXT_PUBLIC_API_URL ||
+  (process.env.NODE_ENV === "production"
+    ? "https://callping-ejn7.onrender.com"
+    : "http://localhost:5000");
     return [
       {
         source: '/api/:path*',
